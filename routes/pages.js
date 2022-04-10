552f3e7c08c072d3525c8a49d9cf7a2e1232a42c
Dylan Wheeler <dwheeler1127@gmail.com>
2022-04-10 17:46:39 -0600

    Set up login and signup pages with server

--- routes/pages.js
+++ routes/pages.js
@@ -1,27 +1,34 @@
 const express = require("express");
-const authController = require('../controllers/auth');
+const authController = require("../controllers/auth");
 
 const router = express.Router();
 
-router.get('/', (req,res) => {
-    res.render('index');
+router.get("/", (req, res) => {
+    res.render("index");
 });
 
-router.get('/register',(req,res) => {
-    res.render('register');
+router.get("/register", (req, res) => {
+    res.render("register");
 });
 
-router.get('/login',(req,res) => {
-    res.render('login');
+router.get("/signup", (req, res) => {
+    res.render("signup", function (err, html) {
+        res.send(html);
+    });
 });
 
-router.get('/mainMenu', authController.isLoggedIn, (req,res) => {
+router.get("/login", (req, res) => {
+    res.render("login", function (err, html) {
+        res.send(html);
+    });
+});
+
+router.get("/mainMenu", authController.isLoggedIn, (req, res) => {
     if (req.user) {
-        res.render('mainMenu');
-    }else {
-
-     res.redirect('/login');   
+        res.render("mainMenu");
+    } else {
+        res.redirect("/login");
     }
 });
 
-module.exports = router; 
+module.exports = router;
--- server.js
+++ server.js
@@ -1,58 +1,58 @@
 const express = require("express");
-const mysql = require('mysql');
-const dotenv = require('dotenv')
-const path = require('path');
-const cookiePasrer = require('cookie-parser');
-
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+const path = require("path");
+const cookieParser = require("cookie-parser");
 
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const gameLogic = require("./gameLogic");
 const io = new Server(server);
 
-dotenv.config({path: './.env'});
+dotenv.config({ path: "./.env" });
 
-const db =  mysql.createConnection({
+const db = mysql.createConnection({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE
+    database: process.env.DATABASE,
 });
 
-const publicDirectory = path.join(__dirname,'./public');
+const publicDirectory = path.join(__dirname, "./public");
 app.use(express.static(publicDirectory));
 
 // Parse URL encoded bodies (as sent by HTML forms)
-app.use(express.urlencoded({ extended:false}));
+app.use(express.urlencoded({ extended: false }));
 // Parse Json bodeis (as sent from HTML forms)
 app.use(express.json());
 
-app.use(cookiePasrer());
+app.use(cookieParser());
 
-app.set('view engine', 'hbs');
+app.engine("html", require("hbs").__express);
+app.set("view engine", "html");
 
-db.connect( (error) => {
-    if (error){
-        console.log(error)
-    }else{
-        var sql = "CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY , name VARCHAR(255) NOT NULL, email VARCHAR(255), password VARCHAR(255) NOT NULL, status VARCHAR(10) DEFAULT 'offline' NOT NULL, games_played INT DEFAULT 0 NOT NULL, games_won INT DEFAULT 0 NOT NULL, games_lost INT DEFAULT 0 NOT NULL, games_drawn INT DEFAULT 0 NOT NULL, win_percentage INT DEFAULT 0 NOT NULL, loss_percentage INT DEFAULT 0 NOT NULL)";  
-        db.query(sql, function (error, result) {  
-        if (error) throw error;  
-        }); 
-        console.log("MySql connected....")
+db.connect((error) => {
+    if (error) {
+        console.log(error);
+    } else {
+        var sql =
+            "CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY , name VARCHAR(255) NOT NULL, email VARCHAR(255), password VARCHAR(255) NOT NULL, status VARCHAR(10) DEFAULT 'offline' NOT NULL, games_played INT DEFAULT 0 NOT NULL, games_won INT DEFAULT 0 NOT NULL, games_lost INT DEFAULT 0 NOT NULL, games_drawn INT DEFAULT 0 NOT NULL, win_percentage INT DEFAULT 0 NOT NULL, loss_percentage INT DEFAULT 0 NOT NULL)";
+        db.query(sql, function (error, result) {
+            if (error) throw error;
+        });
+        console.log("MySql connected....");
     }
-})
+});
 
 // Define Routes
-app.use('/',require('./routes/pages'));
-app.use('/auth',require('./routes/auth'));
+app.use("/", require("./routes/pages"));
+app.use("/auth", require("./routes/auth"));
 
-app.listen(8080,() =>{
-    console.log("Server has started on port 8080")
-})
-
+app.listen(8080, () => {
+    console.log("Server has started on port 8080");
+});
 
 //Test
 
@@ -80,11 +80,11 @@ app.listen(8080,() =>{
 // console.log("Result: " + gameLogic.checkWin(game))
 // console.log(game)
 
-// Game Server 
-io.on('connection', (socket) => {
-    console.log('a user connected');
+// Game Server
+io.on("connection", (socket) => {
+    console.log("a user connected");
 });
-  
+
 server.listen(3000, () => {
-console.log('listening on *:3000');
+    console.log("listening on *:3000");
 });
